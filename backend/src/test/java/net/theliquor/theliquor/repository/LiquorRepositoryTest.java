38647211786d330ac46db49956e5875f1a21a791
redhean <redhean2000@naver.com>
2024-08-24 23:05:29 +0900

    refactor: Use @DataJpaTest

--- backend/src/test/java/net/theliquor/theliquor/repository/LiquorRepositoryTest.java
+++ backend/src/test/java/net/theliquor/theliquor/repository/LiquorRepositoryTest.java
@@ -7,9 +7,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.datasource.init.ScriptUtils;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.core.io.ClassPathResource;
 
@@ -22,41 +24,12 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest
+@DataJpaTest
+@Sql(scripts = "/data.sql")
 @ActiveProfiles("test")
 public class LiquorRepositoryTest {
     @Autowired
     private LiquorRepository liquorRepository;
-
-    @Autowired
-    private DataSource dataSource;
-
-    @BeforeEach
-    public void setUp() throws Exception {
-        System.out.println("BeforeEach");
-        try (Connection conn = dataSource.getConnection()) {
-            // 데이터베이스를 초기 상태로 재설정
-            ScriptUtils.executeSqlScript(conn, new ClassPathResource("data.sql"));
-        }
-    }
-
-    @AfterEach
-    public void tearDown() throws Exception {
-        System.out.println("AfterEach");
-        try (Connection conn = dataSource.getConnection(); Statement stmt = conn.createStatement()) {
-            // 데이터베이스의 참조 무결성 문제를 피하기 위해 순서대로 삭제
-            stmt.execute("SET REFERENTIAL_INTEGRITY FALSE");
-            stmt.execute("TRUNCATE TABLE card_news_images");
-            stmt.execute("TRUNCATE TABLE card_news");
-            stmt.execute("TRUNCATE TABLE images");
-            stmt.execute("TRUNCATE TABLE liquor");
-            stmt.execute("TRUNCATE TABLE brand");
-            stmt.execute("TRUNCATE TABLE producer");
-            stmt.execute("TRUNCATE TABLE classifications");
-            stmt.execute("TRUNCATE TABLE users");
-            stmt.execute("SET REFERENTIAL_INTEGRITY TRUE");
-        }
-    }
 
     @Test
     void LiquorFilterTest() {
