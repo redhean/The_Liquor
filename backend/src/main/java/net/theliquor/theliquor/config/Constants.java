06b7717a1dd64fb670eac9ee0fb46c959b8b23ce
redhean <redhean2000@naver.com>
2024-08-24 17:45:28 +0900

    feat: Constants

---
+++ backend/src/main/java/net/theliquor/theliquor/config/Constants.java
@@ -0,0 +1,7 @@
+package net.theliquor.theliquor.config;
+
+public class Constants {
+    public static final int LIQUOR_SEARCH_PAGE_SIZE = 15;
+    public static final int CARD_NEWS_PAGE_SIZE = 9;
+    public static final int ADMIN_CATEGORY_LIST_SIZE = 20;
+}
