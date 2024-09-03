3e32a87daa16bcc391bc9350e4ca8e7eb389233c
ourgye <cadmium4488@gmail.com>
2024-09-03 16:41:32 +0900

    chore: proxy server for dev mode

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from "path"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-export default defineConfig({
+const defaultConfig = {
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+};
+
+export default defineConfig(({ command, mode }) => {
+  if (command === "serve") {
+    return {
+      ...defaultConfig,
+      server: {
+        proxy: {
+          "/api": {
+            target: 'http://localhost:8080',
+            changeOrigin: true,
+            rewrite: (path) => path.replace(/^\/api/, ''),
+          },
+        },
+      },
+    };
+  } else {
+    return defaultConfig;
+  }
+});
